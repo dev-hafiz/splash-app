06a941bc6a83f9f5900de18a225f36994fc3afdc
okshi <johnstarhafiz@gmail.com>
2022-05-26 15:56:25 +0600

    Complete bottom sheet

--- src/components/bottom-sheet.js
+++ src/components/bottom-sheet.js
@@ -1,30 +1,44 @@
 import { View, StyleSheet, Dimensions } from "react-native";
-import React from "react";
+import React, { useCallback, useEffect, useImperativeHandle } from "react";
 import Text from "./text/text";
 import { colors } from "../theme/colors";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
-import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function BottomSheet() {
   const translateY = useSharedValue(0);
+  const MAX_TRANSLAT_Y = -SCREEN_HEIGHT + 50;
 
-  const gesture = Gesture.Pan().onUpdate((event) => {
-    translateY.value = event.translationY;
+  const context = useSharedValue({ y: 0 });
+  const gesture = Gesture.Pan()
+    .onStart(() => {
+      context.value = { y: translateY.value };
+    })
+    .onUpdate((event) => {
+      translateY.value = event.translationY + context.value.y;
+      translateY.value = Math.max(translateY.value, MAX_TRANSLAT_Y);
+    });
 
+  useEffect(() => {
+    translateY.value = withSpring(-SCREEN_HEIGHT /1.5, { damping: 50 });
+  }, []);
+
+  const rBottomSheetStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateY.value }],
+    };
   });
-
-  const rBottomSheetStyle = useAnimatedStyle( ()=>{
-    return{
-      transform:[{ translateY: translateY.value}]
-    }
-  
-  })
 
   return (
     <GestureDetector gesture={gesture}>
-      <Animated.View style={[styles.bottomSheetContainer,rBottomSheetStyle ]}>
+      <Animated.View style={[styles.bottomSheetContainer, rBottomSheetStyle]}>
         <View style={styles.line} />
       </Animated.View>
     </GestureDetector>
@@ -37,13 +51,13 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: colors.white,
     position: "absolute",
-    top: SCREEN_HEIGHT / 2.5,
+    top: SCREEN_HEIGHT,
     borderRadius: 25,
   },
   line: {
     width: 75,
     height: 4,
-    backgroundColor: colors.purple,
+    backgroundColor: colors.indigo,
     alignSelf: "center",
     marginVertical: 15,
     borderRadius: 4,
